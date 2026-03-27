347c9bcf5447a9199732fc19bdba6c75f8cbe811
Yaswasin Mukunda <yaswasinm22@gmail.com>
2026-03-27 08:19:39 +0530

    Update Habit.js

--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const habitSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   name: {
     type: String,
     required: true,
